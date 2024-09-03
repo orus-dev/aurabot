96d560ef1583e8f028600c1b9af98a48d57aa3bb
Leo dev <klesti.selimay@icloud.com>
2024-09-03 17:16:11 +0200

    ready for deployment

--- install.py
+++ install.py
@@ -6,10 +6,10 @@ if name != 'nt':
     print('installing discord.py')
     cmd('./.venv/bin/pip3 install discord.py')
     cmd('./.venv/bin/pip3 install Flask')
-    token = input('Bot Token>')
-    invite = input('Permanent Invite Link>')
-    with open('config.py', 'w') as f:
-        f.write(f"BOT_TOKEN='{token}'\nINVITE='{invite}'")
+    # token = input('Bot Token>')
+    # invite = input('Permanent Invite Link>')
+    # with open('config.py', 'w') as f:
+    #     f.write(f"BOT_TOKEN='{token}'\nINVITE='{invite}'")
     with open('aura.json', 'w') as f1:
         f1.write('{}')
 else:
