a74a278fa85fb8f2ebeb5ac3e9a0d90cbb4bc1f8
Leo dev <klesti.selimay@icloud.com>
2024-09-03 17:46:46 +0200

    better system for deployment

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ env = os.environ.copy()
 from config import *
 from subprocess import run as run_start
 from threading import Thread
+import time
 
 run = lambda a: run_start(a, env=env)
 t1 = Thread(target=run, args=(['python3', 'invite_sys.py'],))
@@ -12,7 +13,8 @@ t1.daemon = True
 t2.daemon = True
 if USE_INVITE_SERVER:t1.start()
 t2.start()
-# while 1:
+while 1:
+    time.sleep(.5)
 #     # if input() == 'q':
 #     if USE_INVITE_SERVER:t1.join(0)
 #     t2.join(0)
