33e2dd2ffa67ef278fb956d907d66d713592694a
Leo dev <klesti.selimay@icloud.com>
2024-09-03 17:38:18 +0200

    better system for deployment

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 env = os.environ.copy()
-print(env)
-
+import install
 from config import *
 from subprocess import run as run_start
 from threading import Thread
 
 run = lambda a: run_start(a, env=env)
-
-t1 = Thread(target=run, args=(['./.venv/bin/python3', 'invite_sys.py'],))
-t2 = Thread(target=run, args=(['./.venv/bin/python3', 'bot.py'],))
+t1 = Thread(target=run, args=(['python3', 'invite_sys.py'],))
+t2 = Thread(target=run, args=(['python3', 'bot.py'],))
 t1.daemon = True
 t2.daemon = True
 if USE_INVITE_SERVER:t1.start()
